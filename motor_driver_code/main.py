096090f3fcca5201a71791d73d00843985e05f73
Joseph Barchanowicz <joebarchanowicz@gmail.com>
2025-10-02 13:48:05 -0700

    updated and tested main code for two motor drivers and 4 motors

--- motor_driver_code/main.py
+++ motor_driver_code/main.py
@@ -4,34 +4,44 @@ from time import sleep
     
 if __name__ == "__main__":
     motor1 = MotorDriver(12, 10, 11, 13, 14, 15)
+    motor2 = MotorDriver(9, 7, 8, 3, 4, 5)
     try:
         while True:
             motor1.motor_forward(32768)
+            motor2.motor_forward(32768)
             sleep(2)
             
             motor1.motor_stop()
+            motor2.motor_stop()
             sleep(2)
             
             motor1.motor_backward(32768)
+            motor2.motor_backward(32768)
             sleep(2)
             
             motor1.motor_stop()
+            motor2.motor_stop()
             sleep(2)
             
             motor1.motor_fwda_revb(32768)
+            motor2.motor_fwda_revb(32768)
             sleep(2)
             
             motor1.motor_stop()
+            motor2.motor_stop()
             sleep(2)
             
             motor1.motor_reva_fwdb(32768)
+            motor2.motor_reva_fwdb(32768)
             sleep(2)
             
             motor1.motor_stop()
+            motor2.motor_stop()
             sleep(2)
             
     except KeyboardInterrupt:
         motor1.motor_stop()
+        motor2.motor_stop()
         print("Program Stopped")
         
         
